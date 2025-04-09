3a50aa99230d876c3ca4e6157ce47e7329d72551
verm911 <shubham.euonus@gmail.com>
2025-04-09 19:18:17 +0000

    connversion of deta type

---
+++ connversionopresion.js
@@ -0,0 +1,38 @@
+let score = "66aa"
+
+//console.log(typeof score);
+//console.log (typeof(score));
+
+let valueInNumber =Number (score)
+//console.log(typeof  valueInNumber)
+
+//console.log( valueInNumber)
+
+// jese 33 =>  33 cheng kre to esli ho jata hai 
+//. jese 33asdf  y chyeng nhi ho rha    {NaN}    aa rha hai 
+// true => 1   foluse => 0
+
+let isLoggedIn = 66
+
+let booleanisLoggedIn =Boolean(isLoggedIn)
+//console.log  (booleanisLoggedIn)
+
+// 1=> true   0=>false
+//   ""  => false 
+//   shubhjam =>  true 
+
+let  someNumber =333   
+//let stringNumber = String(someNumber)
+//console.log(stringNumber);
+
+//==========================operatiosn==========
+
+
+let value =2
+let neValue = -value
+console.log (neValue)
+
+let str1="shubham"
+let str2=" verma"
+let str3= str1+str2
+console.log(str3);
