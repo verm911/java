7228767a05cc52ff20d4075260a826caa240bf62
verm911 <shubham.euonus@gmail.com>
2025-04-11 10:36:48 +0000

    comprison  s detatype

---
+++ comprison.js
@@ -0,0 +1,13 @@
+// // console.log(1>1);
+// console.log("2" > 1);
+// console.log("02" <1);
+
+//  console.log(null > 0);
+//  console.log(null == 0);
+// console.log(null >= 0);
+// y kbi dekt de skhte hai 
+
+// === y deta type ko bi chek krta hai 
+
+console.log("2" ===2);
+
---
+++ detatyps-smruy.js
@@ -0,0 +1,7 @@
+//  primaitive 
+
+//  7 types    string number booolearn  null undfined symbol gigint  
+
+// refrence  (non primitive)
+
+//array    
