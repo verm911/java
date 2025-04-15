c771ad5f97766c1956ad7c076dcddd674fcdd8a5
verm911 <shubham.euonus@gmail.com>
2025-04-16 00:45:56 +0530

    kiji

---
+++ num-match.js
@@ -0,0 +1,7 @@
+const score = 500
+console.log( score);
+
+
+const balance = new Number (500)
+console.log( balance);
+
--- string.js
+++ string.js
@@ -3,3 +3,37 @@ const repoCount = 50
 
 console.log (`hello my name is ${name} and my name has ${repoCount}`);
 
+
+const gameName = new String("hixft -  hnuigh-fgyu");
+
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+
+console.log(gameName.charAt(10));     // ulta 
+console.log(gameName.indexOf('x'));  // konsi pojisn pe konsa cretor hai 
+
+const newstring = gameName.substring(0, 4)   //   aage se stRT 
+console.log(newstring);
+
+const anthorstring = gameName.slice- ( -7, 4)  // piche se strt 
+console.log(anthorstring);         
+
+ const newstringone ="  iteshgf    "
+ console.log(newstringone);
+ console.log (newstringone.trim());
+
+
+const url = "https://www.gppkl.com/vjgfj%55ujb"
+console.log(url.replace('%55', '-' ));
+
+console.log(gameName.split('-'));
+
+
+
+
+ 
