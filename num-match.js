1a55cbfde642578a74449af074db13cd10c9399d
verm911 <shubham.euonus@gmail.com>
2025-05-01 15:54:56 +0530

    kjgvg

--- num-match.js
+++ num-match.js
@@ -1,7 +1,20 @@
 const score = 500
-console.log( score);
+// console.log( score);
 
 
 const balance = new Number (500)
-console.log( balance);
+// console.log( balance);
 
+    // console.log(balance.toString() .length);     //  ye length btata hai
+    // console.log(balance.toFixed(2) );               // ye 00 bnata hai 
+
+const hundred = 100000000000
+// console.log(hundred .toLocaleString('en-IN'));         // ye 100000000000 ko 1,00,00,00,000 bnata hai
+
+
+
+//================================   maths  ==========================
+
+ console.log(Math);
+
+ 
